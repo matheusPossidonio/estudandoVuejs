7ef4cf36cd31bb5520c7a8391090148aa1c4252e
Matheus <matheuspgvc.mp@gmail.com>
2022-05-30 10:18:47 -0300

    versionando

---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+    <header>
+        <nav class = "container">
+            
+            <a href="/"><img alt="WDEV" id="logo" src ="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/wdev.svg"></a>
+            
+            <img alt="Abrir menu" id = "menu-button" src = "https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg">
+            
+            <div id="menu-overlay"></div>
+
+            <div></div>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default{
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    header{
+        background-color: var(--color-background-nav);
+        width: 100%;
+        height: 60%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    nav{
+        display: flex;
+        justify-content:  space-between;
+    }
+    #logo{
+        width: 130px;
+    }
+    #menu-button{
+        width: 30px;
+    }
+    #menu-overlay{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 40%;
+        height: 100hv;
+        background-color:  #000000;
+        opacity: 0.8;
+    }
+</style>
